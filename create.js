1737dfdf938caf52b774b328c15affa15d3caf2e
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-07-27 08:57:44 -0700

    Created sample handler for create controversy

---
+++ create.js
@@ -0,0 +1,39 @@
+import { success, failure } from '../libs/response-lib';
+
+const
+	AWS = require('aws-sdk'), // eslint-disable-line import/no-extraneous-dependencies
+	dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+AWS.config.update({region:'us-west-1'});
+
+export const create = (event, context, callback) => {
+	const timestamp = new Date().toISOString();
+	const data = JSON.parse(event.body);
+
+	const params = {
+		TableName: process.env.DYNAMODB_TABLE,
+		Item: {
+			slug: data.slug, // partition key
+
+			name: data.name,
+			text: data.text, // array of paragraphs
+			summary: data.summary,
+			author: data.author,
+			original: data.original,
+			category: data.category,
+
+			timestamp,
+			posted: data.posted
+		},
+	};
+
+	// write the listing to the database
+	dynamoDb.put(params, (dbError) => {
+		if (dbError) {
+			console.error(dbError);
+			callback(null, failure(dbError));
+		} else {
+			callback(null, success(params.Item));
+		}
+	});
+};
